7e94f19c0c43bee30be4c4ffcbebcf0cbbe9e989
Adham Hussameldin Ramadan <adhamhussameddin90@gmail.com>
2024-04-03 21:30:57 +0200

    collision check function

--- DungeonSlayer.cpp
+++ DungeonSlayer.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <vector>
 #include <SFML/Graphics.hpp>
 
 using namespace std;
@@ -9,37 +10,35 @@ float deltaTime = 0;
 
 Vector2f velocity = { 0, 0 };
 RenderWindow window(VideoMode(1920, 1080), "Game");
-RectangleShape back;
+
+// Game textures
+
 Texture Player;
 Sprite player;
 Texture room;
 Sprite Room;
+RectangleShape border1(Vector2f({ 150,1080 }));
+RectangleShape border2(Vector2f({ 150,1080 }));
+RectangleShape border3(Vector2f({ 2000,100 }));
+RectangleShape border4(Vector2f({ 1000,100 }));
+RectangleShape border5(Vector2f({ 1000,100 }));
 
 
+
+
+
+// Game functions
 void update();
 void playerMovement();
+void setTextures();
+void checkCollisions();
 void Draw();
 
 
-
-
-
-
-
 int main()
-{
-    
-    room.loadFromFile("Room0.png");
-    Room.setTexture(room);
-    Room.setScale(3.45, 3.45);
-    Room.setOrigin(room.getSize().x/2, room.getSize().y/2);
-    Room.setPosition(1920 / 2 + 50, 1080 / 2 - 100);
-   
-    player.setPosition(200, 200);
-    Player.loadFromFile("Idle.png");
-    player.setTexture(Player);
-    player.setScale(0.125, 0.125);
-    player.setOrigin(Player.getSize().x / 2, Player.getSize().y / 2);
+{   
+    setTextures();
+    window.setMouseCursorVisible(false);
     while (window.isOpen()) {
         // restart the clock at the start of the game loop
         gameClock.restart();
@@ -62,6 +61,7 @@ int main()
 void update()
 {
     playerMovement();
+    checkCollisions();
     player.move(velocity);
 
 }
@@ -99,7 +99,55 @@ void playerMovement()
         velocity.x = 1;
     }
     else {
-
         velocity.x = 0;
     }
+}
+
+void setTextures() 
+{
+    // Room
+    room.loadFromFile("Room0.png");
+    Room.setTexture(room);
+    Room.setScale(3.8, 3.333);
+    Room.setOrigin(room.getSize().x / 2, room.getSize().y / 2);
+    Room.setPosition(1920 / 2 + 50, 1080 / 2 - 100);
+    // Player
+    player.setPosition(500, 500);
+    Player.loadFromFile("Idle.png");
+    player.setTexture(Player);
+    player.setScale(0.125, 0.125);
+    player.setOrigin(Player.getSize().x / 2, Player.getSize().y / 2);
+
+    // walls
+    border2.setPosition(1550, 0);
+    border3.setPosition(0, 1035);
+    border4.setPosition(-150, 150);
+    border5.setPosition(1050, 150);
+
+}
+
+
+void checkCollisions()
+{
+    if (Keyboard::isKeyPressed(Keyboard::A) && player.getGlobalBounds().intersects(border1.getGlobalBounds()))
+    {
+        velocity.x=0;
+    } 
+    if (Keyboard::isKeyPressed(Keyboard::D) && player.getGlobalBounds().intersects(border2.getGlobalBounds()))
+    {
+        velocity.x=0;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::S) && player.getGlobalBounds().intersects(border3.getGlobalBounds()))
+    {
+        velocity.y=0;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::W) && (player.getGlobalBounds().intersects(border4.getGlobalBounds())))
+    {
+        velocity.y=0;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::W) && player.getGlobalBounds().intersects(border5.getGlobalBounds()))
+    {
+        velocity.y = 0;
+    }
+
 }

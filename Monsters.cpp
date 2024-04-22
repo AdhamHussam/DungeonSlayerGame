6a6f542b7f02ae564662b8b776c64331e3d98792
IbrahimYasserM <153026269+IbrahimYasserM@users.noreply.github.com>
2024-04-23 00:07:41 +0200

    making BOD attack
    from now I will be calling bringer of death (BOD)

--- Monsters.cpp
+++ Monsters.cpp
@@ -6,14 +6,17 @@ string walk_path = "enemies/Bringer-Of-Death/Individual Sprite/Walk/Bringer-of-D
 string attack_path = "enemies/Bringer-Of-Death/Individual Sprite/Attack/Bringer-of-Death_Attack_0.png";
 void MonstersMovment(Zombie zombies[], Sprite &Player) {
     double x = Player.getPosition().x - zombies[0].zombie.getPosition().x, y = zombies[0].zombie.getPosition().y - Player.getPosition().y;
-    zombies[0].zombie.move(((x > 0) ? zombies[0].speed : -zombies[0].speed), ((y < 0) ? zombies[0].speed : -zombies[0].speed));
-    if (abs(x) > 100 || true) {
-        counter++;
-        counter %= 800;
+    counter++;
+    counter %= 800;
+    if (x > 0)
+        zombies[0].zombie.setScale(Vector2f(-1, 1));
+    else
+        zombies[0].zombie.setScale(Vector2f(1, 1));
+    if(abs(x) < 100 && abs(y) < 10)
         zombies[0].zombie.setTexture(zombies[0].attack[counter / 100]);
-    }
     else {
-
+        zombies[0].zombie.setTexture(zombies[0].walk[counter / 100]);
+        zombies[0].zombie.move(((x > 0) ? zombies[0].speed : -zombies[0].speed), ((y < 0) ? zombies[0].speed : -zombies[0].speed));
     }
 }
 
@@ -30,6 +33,6 @@ void SetMonsters(Zombie zombies[]) {
         zombies[0].attack[i].loadFromFile(attack_path);
     }
     zombies[0].zombie.setTexture(zombies[0].walk[counter / 100]);
-    zombies[0].zombie.setOrigin(zombies[0].walk[counter / 100].getSize().x / 2, zombies[0].walk[counter / 100].getSize().y / 2);
+    zombies[0].zombie.setOrigin(zombies[0].walk[counter / 100].getSize().x / 4 * 3, zombies[0].walk[counter / 100].getSize().y / 3 * 2);
     zombies[0].zombie.setPosition(-500, 7000);
 }

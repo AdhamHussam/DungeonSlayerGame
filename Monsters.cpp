9fd50a0b71a694fc93f5265f7a50e98a20c3a6f0
IbrahimYasserM <153026269+IbrahimYasserM@users.noreply.github.com>
2024-04-23 09:49:20 +0200

    adding Cast to BOD

--- Monsters.cpp
+++ Monsters.cpp
@@ -1,38 +1,55 @@
 #include "includes.h"
 #include "Monsters.h"
 
-int counter = 0;
+int WalkCounter = 0, AttackCounter = 0, CastCounter = 0, TimeCounter = 0;
+string getBOD(string event, int phase) {
+    return "enemies/Bringer-Of-Death/Individual Sprite/" + event + "/Bringer-of-Death_" + event + "_" + to_string(phase) + ".png";
+}
+void reset_exept(int& place, int mod) {
+    int value = place;
+    WalkCounter = 0;
+    AttackCounter = 0;
+    CastCounter = 0;
+    place = value + 1;
+    place %= mod;
+}
 string walk_path = "enemies/Bringer-Of-Death/Individual Sprite/Walk/Bringer-of-Death_Walk_0.png";
 string attack_path = "enemies/Bringer-Of-Death/Individual Sprite/Attack/Bringer-of-Death_Attack_0.png";
 void MonstersMovment(Zombie zombies[], Sprite &Player) {
     double x = Player.getPosition().x - zombies[0].zombie.getPosition().x, y = zombies[0].zombie.getPosition().y - Player.getPosition().y;
-    counter++;
-    counter %= 800;
     if (x > 0)
         zombies[0].zombie.setScale(Vector2f(-1, 1));
     else
         zombies[0].zombie.setScale(Vector2f(1, 1));
-    if(abs(x) < 100 && abs(y) < 10)
-        zombies[0].zombie.setTexture(zombies[0].attack[counter / 100]);
-    else {
-        zombies[0].zombie.setTexture(zombies[0].walk[counter / 100]);
-        zombies[0].zombie.move(((x > 0) ? zombies[0].speed : -zombies[0].speed), ((y < 0) ? zombies[0].speed : -zombies[0].speed));
+    if(abs(x) < 100 && abs(y) < 10 && !(CastCounter/100)) {
+        // Attack
+        zombies[0].zombie.setTexture(zombies[0].attack[AttackCounter / 100]);
+        reset_exept(AttackCounter, 1000);
     }
+    else if ((long long)abs(x)*abs(x) + abs(y)*abs(y) < 100000 || CastCounter/100) {
+        // Cast
+        zombies[0].zombie.setTexture(zombies[0].Cast[CastCounter / 100]);
+        reset_exept(CastCounter, 900);
+    }
+    else{
+        // Walk
+        zombies[0].zombie.setTexture(zombies[0].walk[WalkCounter / 100]);
+        zombies[0].zombie.move(((abs(x) > 1) ? ((x > 0) ? zombies[0].speed : -zombies[0].speed) : 0), ((y < 0) ? zombies[0].speed : -zombies[0].speed));
+        reset_exept(WalkCounter, 800);
+    }
+    TimeCounter++;
 }
 
 void SetMonsters(Zombie zombies[]) {
     zombies[0].zombie.setPosition(500, 500);
-    for (int i = 0; i < 8; i++){
-        string s = to_string(i + 1);
-        walk_path[70] = s[0];
-        zombies[0].walk[i].loadFromFile(walk_path);
-    }
-    for (int i = 0; i < 9; i++) {
-        string s = to_string(i + 1);
-        attack_path[74] = s[0];
-        zombies[0].attack[i].loadFromFile(attack_path);
-    }
-    zombies[0].zombie.setTexture(zombies[0].walk[counter / 100]);
-    zombies[0].zombie.setOrigin(zombies[0].walk[counter / 100].getSize().x / 4 * 3, zombies[0].walk[counter / 100].getSize().y / 3 * 2);
+    for (int i = 1; i <= 8; i++)
+        zombies[0].walk[i-1].loadFromFile(getBOD("Walk", i));
+    for (int i = 1; i <= 10; i++)
+        zombies[0].attack[i-1].loadFromFile(getBOD("Attack", i));
+    for (int i = 1; i <= 9; i++)
+        zombies[0].Cast[i - 1].loadFromFile(getBOD("Cast", i));
+
+    zombies[0].zombie.setTexture(zombies[0].walk[0]);
+    zombies[0].zombie.setOrigin(zombies[0].walk[0].getSize().x / 4 * 3, zombies[0].walk[0].getSize().y / 3 * 2);
     zombies[0].zombie.setPosition(-500, 7000);
 }
--- Monsters.h
+++ Monsters.h
@@ -4,20 +4,11 @@
 struct Zombie
 {
 	Sprite zombie;
-	Texture walk[8];
-	Texture attack[10];
+	Texture walk[8],attack[10],Cast[9];
+
 	int level;
 	double factor_level = level / 10 + 1;
 	int health = 100*factor_level;
-	int strenth = 10;
+	int strenth = 10*factor_level;
 	double speed = 0.1;
-	void base_atack() {
-
-	}
-	void skill() {
-
-	}
-	void ultimate() {
-
-	}
 };

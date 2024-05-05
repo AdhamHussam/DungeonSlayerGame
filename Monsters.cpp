de76f18a92d54c962131aa4aeccfe947fc306066
Ali-Hady <68974591+Ali-Hady@users.noreply.github.com>
2024-05-05 18:39:09 +0300

    time to move, baby

--- Monsters.cpp
+++ Monsters.cpp
@@ -2,87 +2,9 @@
 #include "Monsters.h"
 #include "globals.h"
 
-const int LENGTH = view.getSize().x;
-const int WIDTH = view.getSize().y;
-
 enum BOD {
     BODwalk, BODattack, BODcast, BODhurt, BODdie
 };
-
-bool inBound(int row, int col) {
-    return (row >= 0 && col >= 0 && row < LENGTH && col < WIDTH);
-}
-
-int heuristic(int startx, int starty, int endx, int endy) {
-    // calculate the heuristic term h essential to A* Algorithm
-    int dx = abs(startx - endx);
-    int dy = abs(starty - endy);
-    int h = 1 * (dx + dy) + (1 - 2 * 1) * min(dx, dy);
-    return h;
-}
-
-// Time to move
-vector<vector<int>> grid(LENGTH, vector<int>(WIDTH));
-// traversing the grid using A* algrithm to find the shortest path 
-ParentsCostPair Astar(int startx, int starty, int endx, int endy) {   
-    for (int x = 0; x < LENGTH; x++) {
-        for (int y = 0; y < WIDTH; y++) {
-            grid[x][y] = 1; // cost of every cell movement
-        }
-    }
-
-    vector<vector<Coordinates>> parents(LENGTH, vector<Coordinates>(WIDTH));
-    vector<vector<int>> cost(LENGTH, vector<int>(WIDTH));
-    for (int idx = 0; idx < LENGTH; idx++) {
-        for (int jdx = 0; jdx < WIDTH; jdx++) {
-            cost[idx][jdx] = INT32_MAX;
-        }
-    }
-    priority_queue<Cell, vector<Cell>, greater<Cell>> pq;
-    Cell start_cell;
-    start_cell.cost = 0;
-    start_cell.node.x = startx;
-    start_cell.node.y = starty;
-    pq.push(start_cell); // Our Monster Location 
-    int rd[] = {0, 0, -1, +1, -1, +1, -1, +1};
-    int cd[] = {-1, +1, 0, 0, -1, +1, +1, -1};
-    cost[startx][starty] = 0;
-
-    while (!pq.empty()) {
-        Cell current_cell = pq.top();
-        int row = current_cell.node.x;
-        int col = current_cell.node.y;
-        int cur_cost = current_cell.cost;
-
-        if (cur_cost > cost[row][col])
-            continue;
-
-        // Visit neighbors of current cell
-        for (int i = 0; i < 8; i++) {
-            
-            int ch_row = row + rd[i]; // child row
-            int ch_col = row + cd[i]; // child column
-            int h = heuristic(ch_row, ch_col, endx, endy);
-            bool is_better_cost = cost[ch_row][ch_col] > cur_cost + grid[ch_row][ch_col] + h;
-
-            if (inBound(ch_row, ch_col) && is_better_cost) {
-                cost[ch_row][ch_col] = cur_cost + grid[ch_row][ch_col] + h;
-                Cell child_cell;
-                child_cell.cost = cost[ch_row][ch_col];
-                child_cell.node = { ch_row, ch_col };
-                parents[ch_row][ch_col] = current_cell.node;
-                pq.push(child_cell);
-
-                if (ch_row == endx && ch_col == endy) {
-                    return { parents, cost };
-                }
-            }
-            
-        }
-
-        return { parents, cost };
-    }
-}
 
 // animation counters
 int MovmentCounter[30];
@@ -106,44 +28,14 @@ void UpdateMonsterAnimationCounter(int i,float st = 0.15){
 }
 
 // make monster walk
-/*
-void walk(int x, int y) {
-    BODmonsters[0].BOD.setTextureRect(getRect(8 + MovmentCounter));
-    
-    BODmonsters[0].BOD.move(((abs(x) > 1) ? ((x > 0) ? BODmonsters[0].speed : -BODmonsters[0].speed) : 0), ((y < 0) ? BODmonsters[0].speed : -BODmonsters[0].speed));
-    
-    sf::Vector2f player_position = Player.getPosition();
-    sf::Vector2f monster_position = BODmonsters[0].BOD.getPosition();
-    ParentsCostPair front = Astar(monster_position.x, monster_position.y, player_position.x + 1, player_position.y);
-    int to_x = front.parents[player_position.x + 1][player_position.y].x;
-    int to_y = front.parents[player_position.x + 1][player_position.y].y;
-    BODmonsters[0].BOD.move(to_x, to_y);
-    //
-    UpdateMonsterAnimationCounter();
-    MovmentCounter %= 8;
-}
-*/
     
 void walk(int x, int y, int i) {
     BODmonsters[i].BOD.setTextureRect(getRect(8 + MovmentCounter[i]));
+    Vector2f Direction = Player.getPosition() - BODmonsters[i].BOD.getPosition();
+    float magnitude = sqrt(Direction.x * Direction.x + Direction.y * Direction.y);
+    Vector2f norm_direction = Direction / magnitude;
+    BODmonsters[i].BOD.move(Vector2f(norm_direction.x * BODmonsters[i].speed * playerdeltatime, norm_direction.y * BODmonsters[i].speed * playerdeltatime));
     //BODmonsters[i].BOD.move(((abs(x) > 1) ? ((x > 0) ? BODmonsters[i].speed*playerdeltatime : -BODmonsters[i].speed* playerdeltatime) : 0), ((y < 0) ? BODmonsters[i].speed* playerdeltatime : -BODmonsters[i].speed* playerdeltatime));
-    sf::Vector2f player_position = Player.getPosition();
-    sf::Vector2f monster_position = BODmonsters[i].BOD.getPosition();
-    ParentsCostPair front = Astar(monster_position.x, monster_position.y, player_position.x + 1, player_position.y);
-    ParentsCostPair back = Astar(monster_position.x, monster_position.y, player_position.x - 1, player_position.y);
-    
-    int to_x, to_y;
-    cout << front.parents[player_position.x + 1][player_position.y].x << '\n';
-    if (inBound(player_position.x + 1, player_position.y)) {
-        // Get the next position from the path
-        to_x = front.parents[player_position.x + 1][player_position.y].x;
-        to_y = front.parents[player_position.x + 1][player_position.y].y;
-    }
-    else {
-        to_x = back.parents[player_position.x - 1][player_position.y].x;
-        to_y = back.parents[player_position.x - 1][player_position.y].y;
-    }
-    BODmonsters[i].BOD.move(to_x, to_y);
     UpdateMonsterAnimationCounter(i);
     MovmentCounter[i] %= 8;
 
--- Monsters.h
+++ Monsters.h
@@ -4,9 +4,8 @@
 struct BringerOfDeath
 {
 	Sprite BOD,spell;
-
 	int health = 10;
-	float speed = 0.1, cooldown = 5;
+	float speed = 100, cooldown = 5;
 };
 
 struct Coordinates

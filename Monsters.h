226dc131a1b84f1e6c37bf2c7352390e38fd65b3
IbrahimYasserM <153026269+IbrahimYasserM@users.noreply.github.com>
2024-05-05 14:26:53 +0300

    trying to merge

--- Monsters.h
+++ Monsters.h
@@ -3,9 +3,30 @@
 
 struct BringerOfDeath
 {
-	Sprite BOD,spell;
+	Sprite BOD, spell;
 
 	int health = 10;
-	int speed = 100;
-	float cooldown = 5;
+	float speed = 0.1, cooldown = 5;
+};
+
+struct Coordinates
+{
+	int x;
+	int y;
+};
+
+struct Cell
+{
+	int cost;
+	Coordinates node;
+
+	bool operator<(const Cell& other) const {
+		return cost > other.cost; // Compare based on cost
+	}
+};
+
+struct ParentsCostPair
+{
+	vector<vector<Coordinates>> parents;
+	vector<vector<int>> cost;
 };

c8a8027c90f8091561b19aa7eefbea7c18babb0b
IbrahimYasserM <153026269+IbrahimYasserM@users.noreply.github.com>
2024-05-09 09:54:51 +0300

    adding max health for AA

--- ArcanArcher.h
+++ ArcanArcher.h
@@ -3,7 +3,7 @@
 
 struct ArcanArcher {
 	Sprite AA,arrow;
-	int health,speed = 300,power = 1,ArrowSpeed = 1000;
+	int health,speed = 300,power = 1,ArrowSpeed = 1000, max_health;
 	float cooldown = 10;
 	bool alive,hitted;
 	Vector2f target;
